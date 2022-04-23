2a95b90bce3de00a7357573185939d2342029c14
comejia <comejia@teco.com.ar>
2022-04-22 23:09:33 -0300

    Fix correcciones

--- src/main/java/com/comejia/dds/negocio/DomainException.java
+++
@@ -1,7 +0,0 @@
-package com.comejia.dds.negocio;
-
-public class DomainException extends RuntimeException {
-    public DomainException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/comejia/dds/prenda/Prenda.java
+++ src/main/java/com/comejia/dds/prenda/Prenda.java
@@ -1,6 +1,6 @@
 package com.comejia.dds.prenda;
 
-import com.comejia.dds.negocio.DomainException;
+import com.comejia.dds.negocio.PrendaInvalidaException;
 
 public class Prenda {
 
@@ -9,27 +9,24 @@ public class Prenda {
     private Color colorPrincipal;
     private Color colorSecundario;
 
-    public Prenda(TipoDePrenda tipoDePrenda, Material material, Color color) {
+    public Prenda(TipoDePrenda tipoDePrenda, Material material, Color colorPrincipal, Color colorSecundario) {
         if (tipoDePrenda == null) {
-            throw new DomainException("Tipo de prenda no valido");
+            throw new PrendaInvalidaException("Tipo de prenda no valido");
         }
         if (material == null) {
-            throw new DomainException("Material no valido");
+            throw new PrendaInvalidaException("Material no valido");
         }
-        if (color == null) {
-            throw new DomainException("Color no valido");
+        if (colorPrincipal == null) {
+            throw new PrendaInvalidaException("Color no valido");
         }
         this.tipoDePrenda = tipoDePrenda;
         this.material = material;
-        this.colorPrincipal = color;
+        this.colorPrincipal = colorPrincipal;
+        this.colorSecundario = colorSecundario;
     }
 
     public Categoria categoria() {
         return tipoDePrenda.categoria();
-    }
-
-    public void setColorSecundario(Color colorSecundario) {
-        this.colorSecundario = colorSecundario;
     }
 
 }
